9d0c0cf4bb4f0fc4b7876594775f2e2cd5ec62dd
JunsuMacbook <krpjs0508@gmail.com>
2022-08-25 08:39:15 +0900

    🧩 [feat] :: implement auto SignIn #1

--- SharingMyWishList/app/src/main/java/com/example/sharingmywishlist/Activity/SignInActivity.java
+++ SharingMyWishList/app/src/main/java/com/example/sharingmywishlist/Activity/SignInActivity.java
@@ -2,7 +2,9 @@ package com.example.sharingmywishlist.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,10 +25,22 @@ public class SignInActivity extends AppCompatActivity {
 
     // TAG
     private final static String TAG = "SignInActivity";
-
     // accessToken
+<<<<<<< Updated upstream
     public static String accessToken;
+=======
+<<<<<<< Updated upstream
+    private static String accessToken;
+>>>>>>> Stashed changes
 
+=======
+    public static String accessToken;
+    // Context
+    private Context context;
+    // SharedPreference
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+>>>>>>> Stashed changes
     // Binding
     private ActivitySignInBinding binding;
 
@@ -36,11 +50,37 @@ public class SignInActivity extends AppCompatActivity {
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // context
+        context = getApplicationContext();
+
+        // SharedPreferences
+        preferences = context.getSharedPreferences("signIn", Context.MODE_PRIVATE);
+        editor = preferences.edit(); // Editor
+
+        // Auto SignIn
+        autoSignIn();
+
+        // TESTLOG
+        Log.d(TAG, "junj auto Login : " + preferences.getBoolean("autoSignIn", false));
+        Log.d(TAG, "junj userId : " + preferences.getString("userId", "empty"));
+        Log.d(TAG, "junj password : " + preferences.getString("password", "empty"));
+
         // Sign In Button ClickListener
         signInButtonClickListener();
 
         // Go To Sign Up ClickListener
         goToSignUpClickListener();
+    }
+
+
+    // Auto Sign In
+    private void autoSignIn() {
+
+        // get Auto SignIn
+        if (preferences.getBoolean("autoSignIn", false)) {
+
+            Log.d(TAG, "autoSignIn called");
+        }
     }
 
 
@@ -73,22 +113,42 @@ public class SignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                // 값을 담아 전달할 String
+                String userId = binding.etSignInUserId.getText().toString(); // userId
+                String password = binding.etSignInPassword.getText().toString(); // password
+                
+                // Set Sign In to Automatically
+                if (binding.chkSignInKeepLoggedIn.isChecked()) {
+
+                    Log.d(TAG, "signInButtonClickListener true");
+
+                    // Put Auto Sign In
+                    editor.putBoolean("autoSignIn", true);
+
+                    // Put user Information
+                    editor.putString("userId", userId); // userId
+                    editor.putString("password", password); // password
+
+                    // commit
+                    editor.commit();
+                }
+
                 // Sign In
-                signIn();
+                signIn(userId, password);
             }
         });
     }
 
 
     // Sign In
-    private void signIn() {
+    private void signIn(String _userId, String _password) {
 
         // log
         Log.d(TAG, "signIn() has called");
 
         // 값을 담을 String
-        String userId = binding.etSignInUserId.getText().toString(); // userId
-        String password = binding.etSignInPassword.getText().toString(); // password
+        String userId = _userId; // userId
+        String password = _password; // password
 
         SignInRequest signInRequest = new SignInRequest(userId, password);
 
