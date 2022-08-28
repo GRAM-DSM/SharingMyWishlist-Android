a67e6f6ee1f6287ec5ef862ee6e6fcd68ca992ce
JunsuMacbook <krpjs0508@gmail.com>
2022-08-28 21:17:37 +0900

    🧩 [feat] :: implement create select color button #2

--- SharingMyWishList/app/src/main/java/com/example/sharingmywishlist/Activity/CreateActivity.java
+++ SharingMyWishList/app/src/main/java/com/example/sharingmywishlist/Activity/CreateActivity.java
@@ -1,10 +1,15 @@
 package com.example.sharingmywishlist.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioGroup;
 
 import com.example.sharingmywishlist.R;
 import com.example.sharingmywishlist.databinding.ActivityCreateBinding;
@@ -26,6 +31,40 @@ public class CreateActivity extends AppCompatActivity {
         // initiate Cancel ClickListener
         initCancelClickListener();
 
+        // initiate Color ClickListener
+        initColorClickListener();
+
+
+    }
+
+
+    // initiate Color ClickListener
+    private void initColorClickListener() {
+
+        Log.d(TAG, "initColorClickListener: ");
+        binding.radioGroupCreate.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+
+                switch (id) {
+                    case R.id.btn_create_nor:
+                        Log.d(TAG, "nor");
+                        break;
+
+                    case R.id.btn_create_red:
+                        Log.d(TAG, "red");
+                        break;
+
+                    case R.id.btn_create_gre:
+                        Log.d(TAG, "blu");
+                        break;
+
+                    case R.id.btn_create_blu:
+                        Log.d(TAG, "nor");
+                        break;
+                }
+            }
+        });
 
     }
 
