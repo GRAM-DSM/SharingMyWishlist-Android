97e3a8524b3f1f6d7c9cac2060546d4b8ac31ea6
JunsuMacbook <krpjs0508@gmail.com>
2022-09-05 23:41:17 +0900

    init detail

--- SharingMyWishListAndroid/app/src/main/java/com/gram2022/sharingmywishlist_android/Detail/DetailActivity.java
+++ SharingMyWishListAndroid/app/src/main/java/com/gram2022/sharingmywishlist_android/Detail/DetailActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gram2022.sharingmywishlist_android.R;
+import com.gram2022.sharingmywishlist_android.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
+
+    ActivityDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
