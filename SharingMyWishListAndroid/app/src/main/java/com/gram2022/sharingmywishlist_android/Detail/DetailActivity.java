bd46acc4063454fd1827827907cba41598b804bd
JunsuMacbook <krpjs0508@gmail.com>
2022-09-05 23:21:43 +0900

    init detail

---
+++ SharingMyWishListAndroid/app/src/main/java/com/gram2022/sharingmywishlist_android/Detail/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.gram2022.sharingmywishlist_android.Detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gram2022.sharingmywishlist_android.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
