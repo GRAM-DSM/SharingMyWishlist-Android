07ecc1fb8fa8ee49e16996deed20d08eb6f3c50a
JunsuMacbook <krpjs0508@gmail.com>
2022-09-09 00:35:12 +0900

    ⚙️[refactor] :: adapter logic

--- SharingMyWishListAndroid/app/src/main/java/com/gram2022/sharingmywishlist_android/Main/ItemAdapter.java
+++ SharingMyWishListAndroid/app/src/main/java/com/gram2022/sharingmywishlist_android/Main/ItemAdapter.java
@@ -2,13 +2,11 @@ package com.gram2022.sharingmywishlist_android.Main;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -52,7 +50,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Log.d(TAG, "onBindViewHolder");
-        holder.bindItem(dataList.get(position), position);
+        holder.bindItem(dataList.get(position));
     }
 
     @Override
@@ -64,23 +62,24 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
         API api = APIProvider.getInstance().create(API.class);
         api.clear(SignInActivity.accessToken, id).enqueue(new Callback<Void>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
                 if (response.isSuccessful()) {
                     Log.d(TAG, "clear() success!");
                 }
             }
 
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
                 Log.d(TAG, "clear() failure..");
             }
         });
     }
+
     private void delete(int id) {
         API api = APIProvider.getInstance().create(API.class);
         api.delete(SignInActivity.accessToken, id).enqueue(new Callback<Void>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
                 if (response.isSuccessful()) {
                     Log.d(TAG, "delete() success!");
                     Toast.makeText(context, R.string.main_delete_success, Toast.LENGTH_SHORT).show();
@@ -90,7 +89,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
             }
 
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
                 Log.d(TAG, "delete() failure..");
                 Toast.makeText(context, R.string.main_delete_failure, Toast.LENGTH_SHORT).show();
             }
@@ -110,7 +109,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
             itemBinding = binding;
         }
 
-        void bindItem(WishAllResponse.WishResponseList item, int position) {
+        void bindItem(WishAllResponse.WishResponseList item) {
 
             Log.d(TAG, "==========");
             Log.d(TAG, "id : " + item.getId());
@@ -129,69 +128,40 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
             itemBinding.tvRvMainItemTitle.setText(item.getTitle());
             itemBinding.tvRvMainItemWriter.setText(item.getWriter());
             itemBinding.tvRvMainItemContents.setText(item.getContents());
-            itemBinding.chkRvMainItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext())
-                            .setTitle(R.string.main_clearDialog)
-                            .setPositiveButton(R.string.main_dialog_positive, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    clear(item.getId());
-                                    disableCheck(itemBinding.chkRvMainItem);
-                                }
-                            })
-                            .setNeutralButton(R.string.main_dialog_neutral, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    setClear(false);
-                                }
-                            })
-                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                @Override
-                                public void onCancel(DialogInterface dialog) {
-                                    setClear(false);
-                                }
-                            });
-                    builder.show();
-                }
+            itemBinding.chkRvMainItem.setOnClickListener(v -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext())
+                        .setTitle(R.string.main_clearDialog)
+                        .setPositiveButton(R.string.main_dialog_positive, (dialog, which) -> {
+                            clear(item.getId());
+                            disableCheck(itemBinding.chkRvMainItem);
+                        })
+                        .setNeutralButton(R.string.main_dialog_neutral, (dialog, which) -> setClear(false))
+                        .setOnCancelListener(dialog -> setClear(false));
+                builder.show();
             });
-            itemBinding.getRoot().setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext())
-                            .setTitle(R.string.main_deleteDialog)
-                            .setPositiveButton(R.string.main_dialog_positive, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    delete(item.getId());
-                                    notifyDataSetChanged();
-                                }
-                            })
-                            .setNeutralButton(R.string.main_dialog_neutral, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                }
-                            })
-                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                @Override
-                                public void onCancel(DialogInterface dialog) {
-                                }
-                            });
-                    builder.show();
-                    return true;
-                }
+
+            itemBinding.getRoot().setOnLongClickListener(v -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext())
+                        .setTitle(R.string.main_deleteDialog)
+                        .setPositiveButton(R.string.main_dialog_positive, (dialog, which) -> {
+                            delete(item.getId());
+                            notifyItemChanged(item.getId());
+                        })
+                        .setNeutralButton(R.string.main_dialog_neutral, (dialog, which) -> {
+                        })
+                        .setOnCancelListener(dialog -> {
+                        });
+                builder.show();
+                return true;
             });
-            itemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent wishDetailIntent = new Intent(context, WishDetailActivity.class);
-                    wishDetailIntent.putExtra("wishId", item.getId());
-                    wishDetailIntent.putExtra("title", item.getTitle());
-                    wishDetailIntent.putExtra("writer", item.getWriter());
-                    wishDetailIntent.putExtra("contents", item.getContents());
-                    context.startActivity(wishDetailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                }
+
+            itemBinding.getRoot().setOnClickListener(v -> {
+                Intent wishDetailIntent = new Intent(context, WishDetailActivity.class);
+                wishDetailIntent.putExtra("wishId", item.getId());
+                wishDetailIntent.putExtra("title", item.getTitle());
+                wishDetailIntent.putExtra("writer", item.getWriter());
+                wishDetailIntent.putExtra("contents", item.getContents());
+                context.startActivity(wishDetailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             });
         }
 
